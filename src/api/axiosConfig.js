d3757eed9b06442c259689a775ebdfb94b7223d2
raiyan3341 <rayanbin13@gmail.com>
2025-12-10 21:13:02 +0600

    done

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://course-enrollment-backend.vercel.app",
+  baseURL: "https://course-enrollment-backend.vercel.app/api",
 });
 
 api.interceptors.request.use((config) => {
